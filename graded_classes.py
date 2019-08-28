62908b5f6b7b169690c0adbbe95deea337e1f11e
autumn-ragland <raglanda998@gmail.com>
2019-08-28 14:20:48 -0500

    Graded 08-28-2019 14:20:48

--- graded_classes.py
+++ graded_classes.py
@@ -1,4 +1,5 @@
 # main function to call all the code that will show up in the terminal
+# !! : make each problem a function
 def main():
     newDog = Dog("Spot", "Chihuahua", "White", "Male")
     newDog.printAttributes()
@@ -6,7 +7,10 @@ def main():
     newProduct = Product("Gum", 1, 60)
     # prints the values assigned from a new instance of Product in an f-string
     print(f"The product name is {newProduct.name}, it costs ${newProduct.price}, and we currently hold {newProduct.quantity} of them.")
-
+    # !! : you should call your functions in the main
+    newProduct.changeName()
+    newProduct.changeNamePrice()
+    newProduct.changeNamePriceQuantity()
 
 class Dog:
     def __init__(self, name, breed, color, gender):
@@ -14,11 +18,9 @@ class Dog:
         self.breed = breed
         self.color = color
         self.gender = gender
-
     # prints the values assigned to a Dog object in an f-string template
     def printAttributes(self):
         print(f"Your dog's name is {self.name}.\nThey are a {self.breed}, their color is {self.color}, and they are {self.gender}.")
-
 
 # quits a while loop if the user enters an = sign
 def quit_func():
@@ -26,29 +28,24 @@ def quit_func():
     while userInput != "=":
         userInput = input("Enter anything at all or press '=' to quit the function: ")
 
-
 class Product:
     def __init__(self, name, price, quantity):
         self.name = name
         self.price = price
         self.quantity = quantity
-
     # each of these methods tell the current value of the name, price, or quantity then allows user to input a new value that is saved into the paramter
     def changeName(self):
         self.name = input(f"The product name is currently {self.name}. What would you like to change the name to?\n")
         print(f"The name is now {self.name}.")
-
     def changeNamePrice(self):
         self.name = input(f"The product name is currently {self.name}. What would you like to change the name to?\n")
         self.price = input(f"The product price is currently ${self.price}. What would you like to change the price to?\n")
         print(f"The name is now {self.name}, and the updated price is ${self.price}.")
-
     def changeNamePriceQuantity(self):
         self.name = input(f"The product name is currently {self.name}. What would you like to change the name to?\n")
         self.price = input(f"The product price is currently ${self.price}. What would you like to change the price to?\n")
         self.quantity = input(f"We have {self.quantity} of this product. What would you like to change the quantity to?\n")
         print(f"The name is now {self.name}, it costs ${self.price} and we hold {self.quantity} of them.")
-
 
 # calls the main function which contains the answers for each problem
 main()
